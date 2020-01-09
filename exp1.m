c5324ea489a6768797f60743692f0edf8ce4f646
5866 <48214118+5866@users.noreply.github.com>
2020-01-09 13:02:16 +0800

    大地坐标与空间直角坐标之间的相互转换

--- exp1.m
+++ exp1.m
@@ -1,8 +1,8 @@
 clear
-%�ó�������ʵ�ִ��������ռ�ֱ������֮����໥ת��
-%��d�̽���һ����Ϊshiyan1���ļ��У���blh.txt����
-% �����������ʵ��һ ����ʦ��ѧԺ ����ѧԺ �ߴ���
-lbh=load('d:/shiyan1/blh1.txt'); %��������
+%该程序用来实现大地坐标与空间直角坐标之间的相互转换
+%在d盘建立一个名为shiyan1的文件夹，将blh.txt放入
+% 测量程序设计实验一 南阳师范学院 环旅学院 高春春
+lbh=load('d:/shiyan1/blh1.txt'); %载入数据
 B=dms2deg(lbh(:,2));
 L=dms2deg(lbh(:,1));
 BLH=[B L lbh(:,3)];
