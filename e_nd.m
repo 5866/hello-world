368245c2dd67de19a082ac26d751e85e4f0bc838
5866 <48214118+5866@users.noreply.github.com>
2020-01-09 12:49:14 +0800

    高斯换带

--- e_nd.m
+++ e_nd.m
@@ -1,14 +1,14 @@
 function [B,L,x,y] = e_nd(ellname,x,y,L0,L0_,k)
-%END »»´ø 194Ò³
-%   k: 3 3¶È´ø
- %     6 6¶È´ø 
+%END æ¢å¸¦ 194é¡µ
+%   k: 3 3åº¦å¸¦
+ %     6 6åº¦å¸¦ 
 
 defval('k',3)
 defval('ellname','kra')
 defval('x',1944359.6070)
 defval('y',240455.4563)
-defval('L0',117) %¾É´ø
-defval('L0_',120) %ĞÂ´ø
+defval('L0',117) %æ—§å¸¦
+defval('L0_',120) %æ–°å¸¦
 
 defval('outputformat','dms')
 
